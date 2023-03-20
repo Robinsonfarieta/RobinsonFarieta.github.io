b86510f82de4c3dd023c23e346f10b236668adfe
Robinsonfarieta <117224867+Robinsonfarieta@users.noreply.github.com>
2023-03-20 15:42:29 -0500

    Add files via upload

---
+++ CRUD Python & Mysql/DB/conexion.py
@@ -0,0 +1,60 @@
+import mysql.connector
+from mysql.connector import Error
+
+### DAO data Access Object - objeto de acesso a datos
+class DAO():
+    def __init__(self):
+        try:
+            self.conexion=mysql.connector.connect(
+                host='localhost',
+                port=3306,
+                user='root',
+                password='12345',
+                db='universidad'
+            )
+        
+        except Error as ex:
+            print("Error al intentar la conexión: {0}".format(ex))
+
+    def listarCursos(self):
+        if self.conexion.is_connected():
+            try:
+                cursor = self.conexion.cursor()
+                cursor.execute("SELECT * FROM curso ORDER BY nombre ASC")
+                resultados = cursor.fetchall()
+                return resultados
+            except Error as ex:
+                print("Error al intentar la conexión: {0}".format(ex))
+
+    def registrarCurso(self, curso):
+        if self.conexion.is_connected():
+            try:
+                cursor = self.conexion.cursor()
+                sql = "INSERT INTO curso (Codigo, Nombre, Creditos) VALUES ('{0}', '{1}', {2})"
+                cursor.execute(sql.format(curso[0], curso[1], curso[2]))
+                self.conexion.commit()
+                print("¡Curso registrado!\n")
+            except Error as ex:
+                print("Error al intentar la conexión: {0}".format(ex))
+
+    def actualizarCurso(self, curso):
+        if self.conexion.is_connected():
+            try:
+                cursor = self.conexion.cursor()
+                sql = "UPDATE curso SET Nombre = '{1}', Creditos = {2} WHERE Codigo = '{0}'"
+                cursor.execute(sql.format(curso[1], curso[2], curso[0]))
+                self.conexion.commit()
+                print("¡Curso actualizado!\n")
+            except Error as ex:
+                print("Error al intentar la conexión: {0}".format(ex))
+
+    def eliminarCurso(self, codigoCursoEliminar):
+        if self.conexion.is_connected():
+            try:
+                cursor = self.conexion.cursor()
+                sql = "DELETE FROM curso WHERE Codigo = '{0}'"
+                cursor.execute(sql.format(codigoCursoEliminar))
+                self.conexion.commit()
+                print("¡Curso eliminado!\n")
+            except Error as ex:
+                print("Error al intentar la conexión: {0}".format(ex))
